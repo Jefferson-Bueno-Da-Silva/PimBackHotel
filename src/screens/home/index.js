f404c3502ee5f6d5145463a4058a8226585a1e87
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-08-30 07:11:58 -0300

    add list on home

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Header } from '../../components';
+import React from 'react'
+import { View, FlatList } from 'react-native'
+import { Header, ListView } from '../../components'
+import { userData, hotelData } from '../../utils/apiMock'
 
-import { styles } from './styles';
+import { styles } from './styles'
 
 const Home = () => {
   return (
-    <View style={styles.container} >
-      <Header/>
+    <View style={styles.container}>
+      <Header uriProfile={userData.profileImage} />
+      <FlatList
+        data={hotelData}
+        keyExtractor={(hotelData) => hotelData.id}
+        renderItem={({ item }) => <ListView data={item} />}
+        style={styles.flatList}
+      />
     </View>
-  );
+  )
 }
 
-export default Home;
+export default Home
--- src/screens/home/styles.js
+++ src/screens/home/styles.js
@@ -1,7 +1,12 @@
 import { StyleSheet } from "react-native";
+import { primary } from "../../styles/colors";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'transparent'
+  },
+  flatList: {
+    backgroundColor: 'transparent'
   },
 })
