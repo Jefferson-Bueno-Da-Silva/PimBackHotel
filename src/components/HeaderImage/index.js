cd129327bdc49822ae3a234cb9bf849183191a37
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-08-30 07:30:14 -0300

    create component HeaderImage

---
+++ src/components/HeaderImage/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { styles } from './styles';
+
+const HeaderImage = () => {
+  return <View style={styles.container} />;
+}
+
+export default HeaderImage;
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Header } from './Header';
 export { default as Avatar } from './Avatar';
 export { default as Input } from './Input';
 export { default as ListView } from './ListView';
+export { default as HeaderImage } from './HeaderImage';
