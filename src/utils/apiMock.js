89a8f0f053b7a436c0cce252f83dec676c81f7bd
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-08-30 07:07:00 -0300

    api

---
+++ src/utils/apiMock.js
@@ -0,0 +1,31 @@
+export const userData = {
+  profileImage : 'https://avatars.githubusercontent.com/u/62211295?v=4',
+  Name: "Jefferson Bueno",
+}
+
+export const hotelData = [
+  {
+    id:"19584dd2-08dc-11ec-9a03-0242ac130003",
+    imagePick: "https://cdn.panrotas.com.br/portal-panrotas-statics/media-files-cache/278526/f8e007f0e5e882a70317c2fb2a43ce7aapartamentoluxoview1/0,89,1280,764/670,400,0.31/0/default.jpg",
+    RoomName: "Ambrosia Hotel & Restaurant",
+    location: "kazi Deiry, Taiger Pass Chittagong",
+  },
+  {
+    id:"21dc2a0a-08dc-11ec-9a03-0242ac130003",
+    imagePick: "https://cdn.panrotas.com.br/portal-panrotas-statics/media-files-cache/278526/f8e007f0e5e882a70317c2fb2a43ce7aapartamentoluxoview1/0,89,1280,764/670,400,0.31/0/default.jpg",
+    RoomName: "Ambrosia Hotel & Restaurant",
+    location: "kazi Deiry, Taiger Pass Chittagong",
+  },
+  {
+    id:"273f209c-08dc-11ec-9a03-0242ac130003",
+    imagePick: "https://cdn.panrotas.com.br/portal-panrotas-statics/media-files-cache/278526/f8e007f0e5e882a70317c2fb2a43ce7aapartamentoluxoview1/0,89,1280,764/670,400,0.31/0/default.jpg",
+    RoomName: "Ambrosia Hotel & Restaurant",
+    location: "kazi Deiry, Taiger Pass Chittagong",
+  },
+  {
+    id:"2a68aafe-08dc-11ec-9a03-0242ac130003",
+    imagePick: "https://cdn.panrotas.com.br/portal-panrotas-statics/media-files-cache/278526/f8e007f0e5e882a70317c2fb2a43ce7aapartamentoluxoview1/0,89,1280,764/670,400,0.31/0/default.jpg",
+    RoomName: "Ambrosia Hotel & Restaurant",
+    location: "kazi Deiry, Taiger Pass Chittagong",
+  },
+]
