3b95a5b9ec5cdc66c5d66c881213d5b7f21c2f93
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-09-07 11:04:34 -0300

    navigate to finished

--- src/screens/Reserve/index.js
+++ src/screens/Reserve/index.js
@@ -1,17 +1,22 @@
-import React, { useState } from 'react';
+import React, { useCallback } from 'react';
 import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/core'
 import { DatePicker, LargeButton, Picker } from '../../components';
 import { hotelData } from '../../utils/apiMock';
 
 import { styles } from './styles';
 
 const Reserve = () => {
+  const navigation = useNavigation()
+  const moveToFinished = useCallback(()=> {
+    navigation.navigate('Finished')
+  }, [])
   return (
     <>
       <DatePicker/>
       <Picker Occupation={hotelData[0].Occupation} />
       <View style={styles.buttonContainer}>
-        <LargeButton onPress={() => {}} buttonText="Confirmar" />
+        <LargeButton onPress={moveToFinished} buttonText="Confirmar" />
       </View>
     </>
   );
