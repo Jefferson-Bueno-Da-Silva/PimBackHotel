4747b4b862ca9d3bc282128c8702d4a9d0cedf0f
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-09-07 11:09:13 -0300

    prettier and expo ConfirmSvg

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,5 @@
-export { default as logo } from './img/logo.png'
-export { default as profile } from './img/profile.jpg'
+export { default as logo } from "./img/logo.png";
+export { default as profile } from "./img/profile.jpg";
+
+// svg
+export { default as ConfirmSvg } from "./svg/ConfirmSvg";
