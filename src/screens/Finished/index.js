61873d2370a79c5556ee50c0467a8266cde3e8df
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-09-07 11:19:52 -0300

    completed "finished" screen

--- src/screens/Finished/index.js
+++ src/screens/Finished/index.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
+import { useNavigation } from "@react-navigation/core";
+import { ConfirmSvg } from "../../assets";
+import { LargeButton } from "../../components";
+
+import { styles } from "./styles";
 
 const Finished = () => {
+  const {navigate} = useNavigation();
+  const navigateToHome = () => {
+    navigate('Home')
+  }
   return (
-    <View>
-      <Text>Finished</Text>
+    <View style={styles.container}>
+      <ConfirmSvg />
+      <Text style={styles.title}>Finalizado</Text>
+      <Text style={styles.paragraph}>
+        Quando chegar no Hotel, faça o check-in pelo celular lendo o QR code.
+      </Text>
+      <View style={styles.buttonContainer}>
+        <LargeButton onPress={navigateToHome} buttonText="Finalizar" />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Finished
+export default Finished;
